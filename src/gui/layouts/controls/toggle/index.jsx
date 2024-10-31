703e4d4837708c93ad5a4fcfdea16ccad03f2406
Iacovlev Alexander <jacoby3210@gmail.com>
2024-10-31 23:56:02 +0200

    add: layouts\controls\toggle

---
+++ src/gui/layouts/controls/toggle/index.jsx
@@ -0,0 +1,50 @@
+import * as gCFG from "/src/gui/config"
+
+// -------------------------------------------------------------------------- //
+// Configuration.
+// -------------------------------------------------------------------------- //
+
+const cfg = gCFG.createConfig({postfix: "toggle"});
+gCFG.applyPackage(cfg, gCFG.propPackageSourceData, {});
+gCFG.applyPackage(cfg, gCFG.propPackageValueBase, {value: null});
+
+// -------------------------------------------------------------------------- //
+// React Component to output multiple radio buttons as a single component.  
+// -------------------------------------------------------------------------- //
+
+const Component = (props) => {
+
+  // initial data
+  
+  const {common, index, item} = props;
+  const {className, ...attributes} = common;
+
+
+  // input handling
+
+  const handleChange = (e) => common.whenValueChange(e.target.value);
+
+  // render
+
+  return (
+    <label 
+      className={`${className.split(" ")[0]}-option`} 
+      key={index}
+    >
+      <input
+        className={`${common.type}-input`}
+        checked={item.value == common.value}
+        name={common.name}
+        type="radio"
+        value={item.value}
+        onChange={handleChange}
+      />
+      {item.label}
+    </label>
+  );
+}
+
+Component.propTypes = cfg.types;
+export const Toggle = {cfg, Component};
+
+// -------------------------------------------------------------------------- //
