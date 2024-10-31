66187e5c75b6196d6dc6d7908238c2693a559482
Iacovlev Alexander <jacoby3210@gmail.com>
2024-11-01 00:21:21 +0200

    add: features\value\text

--- src/gui/features/index.jsx
+++ src/gui/features/index.jsx
@@ -22,7 +22,7 @@ export {withState}              from "./props/state";                         //
 
 export {withValueBase}          from "./value/base";                          // - to handle a change in the value of a component (variant type).
 export {withValueNumber}        from "./value/number";                        // - to handle a change in the value of a component (numeric type).
-// export {withValueHandleText}    from "./value/text";                          //
+export {withValueText}          from "./value/text";                          // - to handle a change in the value of a component (text type).
 // export {withValueHandleItem}   from "./value/withValueHandleItem"         // - to handle a change in the value of a component (source option type).
 
 // -------------------------------------------------------------------------- //
---
+++ src/gui/features/value/text/index.jsx
@@ -0,0 +1,55 @@
+import React, { useCallback } from 'react';
+// -------------------------------------------------------------------------- //
+//  A feature to handle a change in the value of a component (text type).
+// -------------------------------------------------------------------------- //
+
+export const withValueText = (WrappedComponent) => {
+
+  return (receivedProps) => {
+
+    // initial data
+
+    const {
+      valueForbiddenChars: forbidden, 
+      valueLengthMax: max,
+      valueLengthMin: min,
+      value,
+      whenValueChange,
+      whenValueModify,
+    } = receivedProps;
+
+    // supporting methods
+
+    const containsForbiddenChars = (next) => {
+      const forbiddenPattern = new RegExp(`[${forbidden.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')}]`);
+      return forbiddenPattern.test(next);
+    }
+
+    // input handling
+
+    const handleValueChange = useCallback(
+      (next) => {
+        if(!containsForbiddenChars(next)) 
+          whenValueChange(next)
+      },
+      [min, max, whenValueChange]
+    );
+
+    const handleValueModify = useCallback(
+      (increment) =>{ whenValueModify( increment)},
+      [whenValueModify, value]
+    );
+
+	// render 
+  
+    const modifiedProps = {
+      ...receivedProps,
+      whenValueChange: handleValueChange,
+      whenValueModify: handleValueModify,
+    };
+
+    return <WrappedComponent {...modifiedProps} />;
+  };
+};
+
+// -------------------------------------------------------------------------- //
