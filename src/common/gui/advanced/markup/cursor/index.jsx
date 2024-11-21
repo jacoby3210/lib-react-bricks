c0c0db422efa2d3b5fce792470e667051ad49983
Alexandr <jacoby3210@gmail.com>
2024-11-21 16:43:58 +0200

    fix: src\common\gui\advanced\markup\cursor - handleKeyDown is correct

--- src/common/gui/advanced/markup/cursor/index.jsx
+++ src/common/gui/advanced/markup/cursor/index.jsx
@@ -28,6 +28,11 @@ export const withCursor = (WrappedComponent) => (props) => {
   );
 
   // input handling
+
+  const handleClick = useCallback(
+    (evt) => whenValueChange(evt.target.value),
+    [whenValueChange]
+  );
   
   const handleKeyDown = useCallback(
     (evt) => {
@@ -37,14 +42,8 @@ export const withCursor = (WrappedComponent) => (props) => {
         if (evt.key === 'Enter')      setSelectedValue(matchingItems[prev]?.id);
         return prev;
       });
-      evt.preventDefault();
     },
     [matchingItems]
-  );
-
-  const handleClick = useCallback(
-    (evt) => whenValueChange(evt.target.value),
-    [whenValueChange]
   );
 
   // render
