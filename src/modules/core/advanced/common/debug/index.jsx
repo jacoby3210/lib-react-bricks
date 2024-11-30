925aed97ed31cfb561d05540fd45ee526c63210a
Iacovlev Alexander <jacoby3210@gmail.com>
2024-12-01 01:45:06 +0200

    add: \src\modules\core\advanced\common\debug - to display the element name in the react developer tools.

---
+++ src/modules/core/advanced/common/debug/index.jsx
@@ -0,0 +1,26 @@
+
+// -------------------------------------------------------------------------- //
+//  A feature - to display the element name in the react developer tools.
+// -------------------------------------------------------------------------- //
+
+export const withDebug = (name = "Component", ...hocs) => 
+
+  (BaseComponent) => {
+    
+    return hocs.reduceRight((AccumulatedComponent, hoc) => {
+      
+      const updateDisplayName = () => {
+        const hocName = hoc.name || "AnonymousHOC";
+        const baseName = AccumulatedComponent?.displayName || BaseComponent?.displayName || name;
+        return `${hocName}(${baseName})`;
+      }
+
+      const EnhancedComponent = hoc(AccumulatedComponent);
+      EnhancedComponent.displayName = updateDisplayName();
+      return EnhancedComponent;
+
+    }, BaseComponent);
+
+  };
+
+// -------------------------------------------------------------------------- //
---
+++ src/modules/core/advanced/index.jsx
@@ -0,0 +1,16 @@
+
+// -------------------------------------------------------------------------- //
+// Advanced GUI features implemented as HOCs.
+// -------------------------------------------------------------------------- //
+
+// common - аdd a few general capabilities.
+
+export {withDebug}                    from "./common/debug";                  // - to display the element name in the react developer tools.
+
+// markup - modifies and tracks the html markup of the component.
+
+// props - modifies and tracks the properties of the component.
+
+// value 
+
+// -------------------------------------------------------------------------- //
