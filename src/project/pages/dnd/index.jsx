b2d182f8cf9d2f08a8734160d5332b5d675784e2
Iacovlev Alexander <jacoby3210@gmail.com>
2024-12-12 20:52:17 +0200

    add: \src\project\pages\dnd - showcase dnd module.

---
+++ src/project/pages/dnd/index.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useRef, useState } from "react";
+// import { DnD } from "@lib-react-bricks/src/modules/dnd";
+
+// -------------------------------------------------------------------------- //
+// React Component
+// -------------------------------------------------------------------------- //
+
+export const PageDnD = (receivedProps) => {
+  return <></>;
+};
+
+// -------------------------------------------------------------------------- //
--- src/project/pages/router.jsx
+++ src/project/pages/router.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import "@lib-react-bricks/src/assets/themes/theme.css";
 import { PageCoreComponents } from "./core/components";
 import { PageCoreLayouts } from "./core/layouts";
-
+import { PageDnD } from "./dnd";
 // ------------------------------------------------------------------------- //
 // Main Application Router.
 // Using router to control navigation through application pages.
@@ -14,7 +14,8 @@ function Router() {
     <Routes>
       <Route element={<PageCoreComponents />} path='/core/components' exact />
       <Route element={<PageCoreLayouts />} path='/core/layouts/' exact />
-      <Route element={<PageCoreLayouts />} path='/' exact />
+      <Route element={<PageDnD />} path='/dnd' exact />
+      <Route element={<PageDnD />} path='/' exact />
     </Routes>
   );
 }
