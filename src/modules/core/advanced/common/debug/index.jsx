43849f4c794301c97b1a05e25d0270058ae96a11
Iacovlev Alexander <jacoby3210@gmail.com>
2024-12-22 18:41:31 +0200

    update: src\modules\core\advanced\common\debug - code cleanup.

--- src/modules/core/advanced/common/debug/index.jsx
+++ src/modules/core/advanced/common/debug/index.jsx
@@ -3,20 +3,25 @@
 // -------------------------------------------------------------------------- //
 
 export const withDebug =
-  (name = "Component", ...hocs) =>
+  (name = "Component", ...HOCs) =>
   (BaseComponent) => {
-    return hocs.reduceRight((AccumulatedComponent, hoc) => {
+    return HOCs.reduceRight((AccumulatedComponent, hoc) => {
       const updateDisplayName = () => {
         const hocName = hoc.name || "AnonymousHOC";
         const baseName =
           AccumulatedComponent?.displayName ||
           BaseComponent?.displayName ||
           name;
+
         return `${hocName}(${baseName})`;
       };
 
       const EnhancedComponent = hoc(AccumulatedComponent);
       EnhancedComponent.displayName = updateDisplayName();
+
+      console.debug(
+        `[withDebug] Applied HOC: ${EnhancedComponent.displayName}`
+      );
       return EnhancedComponent;
     }, BaseComponent);
   };
